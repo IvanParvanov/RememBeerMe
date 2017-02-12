2c4a60e31cad8e9b2f34f3727a8f9ab4acb47864
J0hnyBG <ivan.parvanov1@gmail.com>
2017-02-12 19:01:49 +0200

    new unit tests

---
+++ RememBeer.Tests/Business/Mocks/MockedBreweriesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+using RememBeer.Business.Admin.Breweries;
+using RememBeer.Models.Contracts;
+
+namespace RememBeer.Tests.Business.Mocks
+{
+    public class MockedBreweriesViewModel : BreweriesViewModel
+    {
+        public override IEnumerable<IBrewery> Breweries { get; set; }
+    }
+}
